872751b00971bc6d43d0a93c2db196fe0c67d10a
muhammad iqbal <muhammadiqbal.rpl@gmail.com>
2022-08-29 15:50:40 +0700

    remove unused

--- src/service/bankAccount.js
+++ src/service/bankAccount.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-// export const getBankAccount = () => getWithoutHeader('fakebank/accounts')
-
 export const getBankAccount = (params) => {
   return axios.get('https://api.sampleapis.com/fakebank/accounts', params)
 };
